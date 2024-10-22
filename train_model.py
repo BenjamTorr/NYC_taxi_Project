e8b1d35d3429befd65530ab7bb9fcea28350e7d2
Benjamin Torres <ibta@unc.edu>
2024-10-22 16:34:08 -0400

    Add arguments to run function and saved a model

--- train_model.py
+++ train_model.py
@@ -4,12 +4,21 @@ import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 from scripts.model_utils import *
 from scripts.utils import *
+import random
+import argparse
+import numpy as np
 
-def main():
-    """Simple training loop"""
+def main(learning_rate, num_epochs):
+    """
+    Simple training loop
+
+    """
     # Set fixed random number seed
     torch.manual_seed(42)
-  
+#    np.random.seed(42)
+    random.seed(42)
+
+
     # load data
     raw_df = raw_taxi_df(filename="data\\yellow_tripdata_2024-01.parquet")
     clean_df = clean_taxi_df(raw_df=raw_df)
@@ -28,11 +37,11 @@ def main():
   
     # Define the loss function and optimizer
     loss_function = nn.L1Loss()
-    optimizer = torch.optim.Adam(mlp.parameters(), lr=1e-4)
+    optimizer = torch.optim.Adam(mlp.parameters(), lr=learning_rate)
   
     # Run the training loop
-    for epoch in range(0, 5): # 5 epochs at maximum
-        print(f'Starting epoch {epoch+1}')
+    for epoch in range(0, num_epochs): # 5 epochs at maximum
+        print(f'-------------------Starting epoch {epoch+1}----------------------')
         current_loss = 0.0
     
         # Iterate over the DataLoader for training data
@@ -60,11 +69,23 @@ def main():
             # Print statistics
             current_loss += loss.item()
             if i % 10 == 0:
-                print('Loss after mini-batch %5d: %.3f' % (i + 1, current_loss / 500))
+                print('Loss after mini-batch %5d: %.3f' % (i + 1, current_loss / 500) + ' in epoch ' + str(epoch + 1))
             current_loss = 0.0
     # Process is complete.
     print('Training process has finished.')
+
+    torch.save(mlp.state_dict(), f'models/trained_model_lr_{learning_rate}_epochs_{num_epochs}.pth')
+    print('Model is saved in models folder.')
     return X_train, X_test, y_train, y_test, data, mlp
 
 if __name__ == "__main__":
-    main()
+    # Initialize the argument parser
+    parser = argparse.ArgumentParser(description="Train a model with specified hyperparameters")
+
+    # Add arguments for hyperparameters
+    parser.add_argument('--learning_rate', type=float, default=1e-4, help='Learning rate for the optimizer')
+    parser.add_argument('--num_epochs', type=int, default=5, help='Number of training epochs')
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+    main(args.learning_rate, args.num_epochs)
